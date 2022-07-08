5fe9a50208a0a605030c92f9cea2448eb6d59eaf
lyk-brave <officeyongki@gmail.com>
2022-07-08 10:17:26 +0900

    complete

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var login = require('../lib/login');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+
+  console.log(req.session);
+  res.render('index', {
+    title: 'Express',
+    loginStatusUI: login.loginStatusUI(req, res),
+  })
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+router.post('/login_process', (req, res, next) => {
+  let post = req.body;
+
+  let email = post.email;
+  let password = post.password;
+  let expires_year = 60 * 60 * 24 * 365;
+  
+  req.session.is_logined = true;
+  req.session.ss_email = email; 
+
+  req.session.save(function () {
+    res.redirect(`/`);
+  });
+});
+
+router.get('/logout_process', (req, res, next) => {
+  req.session.destroy(function(err){
+    res.redirect('/');
+  });
+});
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', (req, res, next) => {
+  res.render('users/login', { title: 'Express - Login' });
 });
 
 module.exports = router;
